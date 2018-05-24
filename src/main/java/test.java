9e55d13b0a0f723e4d2be8936b8ec385897d3ba1
zx3620 <zx3620@gmail.com>
2018-05-24 11:16:19 +0900

    2018.05.24

---
+++ src/main/java/test.java
@@ -0,0 +1,9 @@
+
+public class test {
+public static void main(String[] args) {
+    for(int i = 0; i < 1000; i++) {
+    System.out.println("코딩 애브리바디 "+ i +"</br>");
+    System.out.println(i);
+    }
+}
+}
