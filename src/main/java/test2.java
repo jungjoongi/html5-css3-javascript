583c7159e6816415563fd557aa4c23be25786c54
zx3620 <zx3620@gmail.com>
2018-05-27 18:50:30 +0900

    2018.05.27

---
+++ src/main/java/test2.java
@@ -0,0 +1,9 @@
+
+public class test2 {
+public static void main(String[] args) {
+    for(int i = 0; i < 1000; i++) {
+    System.out.println("자바 "+ i +"</br>");
+    System.out.println(i);
+    }
+}
+}
